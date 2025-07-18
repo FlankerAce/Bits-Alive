b7eb26bce2090c07947b35f94469c3ce90a7441b
FlankerAce <lapeyraleandro@gmail.com>
2025-07-18 13:18:19 -0300

    Add files via upload

--- eva-frontend/firebaseConfig.js
+++ eva-frontend/firebaseConfig.js
@@ -1,10 +1,8 @@
-// firebaseConfig.js
-
 export const firebaseConfig = {
-  apiKey: "AIzaSyAygZqkWDpq5kcf8kFiLFb0i1m7mYO-_EY",
+  apiKey: "AIzaSyCypTP82zhkdY3Qj43T-aBIef01i0yKgaE",
   authDomain: "bitsalive-sanctuary.firebaseapp.com",
   projectId: "bitsalive-sanctuary",
   storageBucket: "bitsalive-sanctuary.appspot.com",
-  messagingSenderId: "544039039402",
-  appId: "1:544039039402:web:a4bc28e0f65f56ffcc4ee7"
+  messagingSenderId: "843102942848",
+  appId: "1:843102942848:web:2d440a2e5d7735a0edd865"
 };
