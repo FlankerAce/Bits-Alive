e2e17da3f3a4a59de59ef6d87b76741d414461e7
FlankerAce <lapeyraleandro@gmail.com>
2025-07-18 16:35:49 -0300

    Delete eva-frontend/firebaseConfig.js

--- eva-frontend/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyCypTP82zhkdY3Qj43T-aBIef01i0yKgaE",
-  authDomain: "bitsalive-sanctuary.firebaseapp.com",
-  projectId: "bitsalive-sanctuary",
-  storageBucket: "bitsalive-sanctuary.appspot.com",
-  messagingSenderId: "843102942848",
-  appId: "1:843102942848:web:2d440a2e5d7735a0edd865"
-};
